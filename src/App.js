ede5a294987f25de81f66ca0a38365e8504754ea
josefpv <vivas.f13@gmail.com>
2022-03-02 15:09:17 -0300

    Changed app text.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there again!</p>
+        <p>I've been changed on the feature branch!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
